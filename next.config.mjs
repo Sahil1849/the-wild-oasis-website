0f95735a3f6202ec21cee161331254913d9b69b9
sahilbankar-17 <sahilbankarvio@gmail.com>
2024-11-30 15:47:57 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["rizcppdtktlcuzwcwpbp.supabase.co"],
+  },
+};
 
 export default nextConfig;
